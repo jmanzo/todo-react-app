8fb4c9d1f1dfa50051a97b748dfcf2f291960491
Jean Manzo <jdevmanzo@gmail.com>
2024-09-15 20:00:00 -0500

    Initial structure of todo list web app

---
+++ src/TodoCounter.js
@@ -0,0 +1,5 @@
+function TodoCounter() {
+  return <h1>Has completado 2 de 3 TODOs</h1>;
+}
+
+export { TodoCounter };
---
+++ src/TodoItem.js
@@ -0,0 +1,11 @@
+function TodoItem() {
+  return (
+    <li>
+        <span>C</span>
+        <span>Tarea</span>
+        <span>X</span>
+    </li>
+  );
+}
+
+export { TodoItem };
